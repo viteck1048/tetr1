18321311bd859bce5f942e4418276b6e3a9abd04
viteck1048 <111514416+viteck1048@users.noreply.github.com>
2022-08-18 16:39:47 +0300

    Add files via upload

---
+++ tetr.h
@@ -0,0 +1,19 @@
+void rot(int n_fig_p);
+void gen();
+void prnt(int ff);
+int scan();
+void setup(int);
+int prov_lin();
+int prov_krok(int new_x, int new_y);
+int prov_rot(int new_fig_p);
+void prnt_podg(int ff);
+int loop();
+void titry();
+void gen_vaj();
+void set_adv();
+
+#define V_LIN 22
+#define H_LIN 11
+#define PRR 2
+#define pryskorennia 200
+#define shvydkist 1000 
